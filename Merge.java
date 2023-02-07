74058aa8b37ebd5c168193901be9e0424aa18617
JPS4321 <sol22102@uvg.edu.gt>
2023-02-06 23:20:13 -0600

    -

--- Merge.java
+++ Merge.java
@@ -1,22 +1,22 @@
 public class Merge {
 
     void merge(int[] array, int u, int w, int v){
-        int n1 = w- u +1;
-        int n2 = v - u;
+        int n1 = w - u  +1;
+        int n2 = v - w;
 
         int [] U = new int[n1];
-        int V[] = new int[n2];
+        int[] V = new int[n2];
 
 
-        for (int i = 0; i < n1; i++)
+        for (int i = 0; i < n1; ++i)
             U[i] = array[u + i];
 
-        for (int j = 0; j < n2; j++)
+        for (int j = 0; j < n2; ++j)
             V[j] = array[w + 1 + j];
 
         int i=0, j=0;
 
-        int p = 1;
+        int p = u;
 
         while (i < n1 && j < n2) {
             if (U[i] <= V[j]) {
